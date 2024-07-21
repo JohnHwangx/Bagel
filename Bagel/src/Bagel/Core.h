44c5bbff40ccfc916e32947cbd9bd1d88bc62f09
huangqiang <johnhuangx@outlook.com>
2024-07-21 17:36:31 +0800

    set static lib

--- Bagel/src/Bagel/Core.h
+++ Bagel/src/Bagel/Core.h
@@ -1,11 +1,17 @@
 #pragma once
 
 #ifdef BG_PLATFORM_WINDOWS
-	#ifdef BG_BUILD_DLL
-		#define BAGEL_API __declspec(dllexport)
+	#ifdef BG_DYNAMIC_LINK
+		#ifdef BG_BUILD_DLL
+			#define BAGEL_API __declspec(dllexport)
+		#else
+			#define BAGEL_API __declspec(dllimport)
+		#endif
 	#else
-		#define BAGEL_API __declspec(dllimport)
-	#endif
+		#define BAGEL_API 
+	#endif // BG_DYNAMIC_LINK
+
+	
 #else
 	#error Bagel only supports Windows!
 #endif
