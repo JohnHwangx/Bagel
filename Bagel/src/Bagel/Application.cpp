41ee4e48f2834d09fb240c48e274225f56e2026b
huang qiang <johnhuangx@outlook.com>
2024-07-26 16:32:04 +0800

    add Event and Test

--- Bagel/src/Bagel/Application.cpp
+++ Bagel/src/Bagel/Application.cpp
@@ -1,18 +1,27 @@
 #include "Application.h"
 
+#include "Bagel/Log.h"
+#include "Events/ApplicationEvent.h"
+//#include "Events/KeyEvent.h"
+#include <iostream>
+
 namespace Bagel {
-    Application::Application()
-    {
+	Application::Application()
+	{
 
-    }
+	}
 
-    Application::~Application()
-    {
+	Application::~Application()
+	{
 
-    }
+	}
 
-    void Application::Run()
-    {
-        while (true);
-    }
+	void Application::Run()
+	{
+		WindowResizeEvent e(1280, 720);
+		//std::cout << e << std::endl;
+		BG_TRACE(e.ToString());
+
+		while (true);
+	}
 }
--- Bagel/src/Bagel/Log.h
+++ Bagel/src/Bagel/Log.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "Core.h"
-#include "spdlog/spdlog.h"
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
 
 namespace Bagel {
 	class BAGEL_API Log
@@ -21,10 +22,10 @@ namespace Bagel {
 #define BG_CORE_INFO(...)	::Bagel::Log::GetColorLogger()->info(__VA_ARGS__)
 #define BG_CORE_WARN(...)	::Bagel::Log::GetColorLogger()->warn(__VA_ARGS__)
 #define BG_CORE_ERROR(...)	::Bagel::Log::GetColorLogger()->error(__VA_ARGS__)
-#define BG_CORE_FATAL(...)	::Bagel::Log::GetColorLogger()->fatal(__VA_ARGS__)
+#define BG_CORE_FATAL(...)	::Bagel::Log::GetColorLogger()->critical(__VA_ARGS__)
 
 #define BG_TRACE(...)		::Bagel::Log::GetClinetLogger()->trace(__VA_ARGS__)
 #define BG_INFO(...)		::Bagel::Log::GetClinetLogger()->info(__VA_ARGS__)
 #define BG_WARN(...)		::Bagel::Log::GetClinetLogger()->warn(__VA_ARGS__)
 #define BG_ERROR(...)		::Bagel::Log::GetClinetLogger()->error(__VA_ARGS__)
-#define BG_FATAL(...)		::Bagel::Log::GetClinetLogger()->fatal(__VA_ARGS__)
+#define BG_FATAL(...)		::Bagel::Log::GetClinetLogger()->critical(__VA_ARGS__)
