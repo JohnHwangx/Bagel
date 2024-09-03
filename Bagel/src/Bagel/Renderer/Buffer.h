63aa23ae22fa7777180fa544fec833fa734d3cc0
huang qiang <johnhuangx@outlook.com>
2024-09-03 10:52:02 +0800

    Add vertex buffer layout abstract

--- Bagel/src/Bagel/Renderer/Buffer.h
+++ Bagel/src/Bagel/Renderer/Buffer.h
@@ -1,6 +1,98 @@
-#pragma once
+﻿#pragma once
 
 namespace Bagel {
+
+	enum class ShaderDataType
+	{
+		None = 0, Float, Float2, Float3, Float4, Mat3, Mat4, Int, Int2, Int3, Int4, Bool
+	};
+
+	static uint32_t ShaderDataTypeSize(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case Bagel::ShaderDataType::Float:	return 4;
+		case Bagel::ShaderDataType::Float2:	return 4 * 2;
+		case Bagel::ShaderDataType::Float3:	return 4 * 3;
+		case Bagel::ShaderDataType::Float4:	return 4 * 4;
+		case Bagel::ShaderDataType::Mat3:	return 4 * 3 * 3;
+		case Bagel::ShaderDataType::Mat4:	return 4 * 4 * 4;
+		case Bagel::ShaderDataType::Int:	return 4;
+		case Bagel::ShaderDataType::Int2:	return 4 * 2;
+		case Bagel::ShaderDataType::Int3:	return 4 * 3;
+		case Bagel::ShaderDataType::Int4:	return 4 * 4;
+		case Bagel::ShaderDataType::Bool:	return 1;
+		}
+		BG_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
+	struct BufferElement
+	{
+		std::string Name;
+		ShaderDataType Type;
+		uint32_t Size;
+		uint32_t Offset;
+		bool Normalized;
+
+		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
+			:Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
+		{}
+
+		uint32_t GetComponenetCount() const
+		{
+			switch (Type)
+			{
+			case Bagel::ShaderDataType::Float:	return 1;
+			case Bagel::ShaderDataType::Float2:	return 2;
+			case Bagel::ShaderDataType::Float3:	return 3;
+			case Bagel::ShaderDataType::Float4:	return 4;
+			case Bagel::ShaderDataType::Mat3:	return 3 * 3;
+			case Bagel::ShaderDataType::Mat4:	return 4 * 4;
+			case Bagel::ShaderDataType::Int:	return 1;
+			case Bagel::ShaderDataType::Int2:	return 2;
+			case Bagel::ShaderDataType::Int3:	return 3;
+			case Bagel::ShaderDataType::Int4:	return 4;
+			case Bagel::ShaderDataType::Bool:	return 1;
+			}
+			BG_CORE_ASSERT(false, "Unknown ShaderDataType!");
+			return 0;
+		}
+	};
+
+	// 顶点缓冲布局抽象类
+	class BufferLayout {
+	public:
+		BufferLayout() {}
+		// 用初始化列表构造BufferLayout对象
+		BufferLayout(const std::initializer_list<BufferElement>& elements)
+			: m_Elements(elements)
+		{
+			CalculateOffsetsAndStride();
+		}
+		inline uint32_t GetStride() const { return m_Stride; }// 获取属性列表总大小
+		inline const std::vector<BufferElement>& GetElements() const { return m_Elements; }// 获取元素列表
+		// 写vector的begin与end，以便循环BufferLayout类 for (const auto& element : BufferLayout)
+		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
+		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+		std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
+		std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
+	private:
+		//
+		// 计算属性列表各个属性的偏移量，以及总大小//
+		void CalculateOffsetsAndStride() {
+			uint32_t offset = 0;
+			m_Stride = 0;
+			for (auto& element : m_Elements) {
+				element.Offset = offset;
+				offset += element.Size;
+				m_Stride += element.Size;
+			}
+		}
+	private:
+		std::vector<BufferElement> m_Elements;
+		uint32_t m_Stride = 0;
+	};
 
 	class VertexBuffer
 	{
@@ -8,6 +100,9 @@ namespace Bagel {
 		virtual ~VertexBuffer() {}
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
+
+		virtual const BufferLayout& GetLayout() const = 0;
+		virtual void SetLayout(const BufferLayout& layout) = 0;
 
 		static VertexBuffer* Create(float* vertices, uint32_t size);
 	};
