08fe472e3e43361fac7322b9aa8adb8b9d6a20c0
huang qiang <johnhuangx@outlook.com>
2024-09-13 09:25:30 +0800

    Added proper window resize handling

--- Bagel/src/Bagel/Renderer/RendererAPI.h
+++ Bagel/src/Bagel/Renderer/RendererAPI.h
@@ -15,6 +15,7 @@ namespace Bagel {
 
 	public:
 		virtual void Init() = 0;
+		virtual void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) = 0;
 		virtual void SetClearColor(const glm::vec4& color) = 0;
 		virtual void Clear() = 0;
 		virtual void DrawIndex(const std::shared_ptr<VertexArray>& vertexArray) = 0;
